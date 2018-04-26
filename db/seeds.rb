e560cc413dfe17d61e783cae1481fa8493c65717
Isaac Powell <garethclyperis@gmail.com>
2018-04-26 11:55:14 +1000

    initial

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+products = Product.create([
+  { name: 'iPhone X',
+    description: 'Face thingy, dumb AI, Notch Pro, Fast, Attractive',
+    price: 1200
+  },
+  { name: 'Nintendo Switch',
+    description: 'Cute controllers, colors, Labo, Kickass games',
+    price: 559
+  },
+  { name: 'Sega Megadrive',
+    description: 'Best controller, Alex Kidd built in, Great sounds',
+    price: 800
+  }
+  ])
